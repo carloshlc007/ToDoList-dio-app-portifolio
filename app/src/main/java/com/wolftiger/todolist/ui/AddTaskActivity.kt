ab721cb3ba54ffd321cfb61a8885c80afda621ea
carlos h l cruz <carloshlc@hotmail.com>
2021-09-12 00:42:14 -0300

    time picker

--- app/src/main/java/com/wolftiger/todolist/ui/AddTaskActivity.kt
+++ app/src/main/java/com/wolftiger/todolist/ui/AddTaskActivity.kt
@@ -3,6 +3,8 @@ package com.wolftiger.todolist.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.wolftiger.todolist.databinding.ActivityAddTaskBinding
 import com.wolftiger.todolist.extensions.format
 import com.wolftiger.todolist.extensions.text
@@ -30,6 +32,17 @@ class AddTaskActivity : AppCompatActivity() {
                 binding.tilDate.text = Date(it + offset).format()
             }
             datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+        }
+
+        binding.tilHour.editText?.setOnClickListener{
+            val timerPicker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .build()
+
+            timerPicker.addOnPositiveButtonClickListener{
+                binding.tilHour.text = "${timerPicker.hour}:${timerPicker.minute}"
+            }
+            timerPicker.show(supportFragmentManager, null)
         }
     }
 }
